24bdfb545b590e86ac90070549fd3a49a6b95c9b
Mizere Robin Roland <mizererobin161@PETOFI-ASZOD.LAN>
2024-10-04 13:11:24 +0200

    Minor Changes
    - Menu system refactor
    - User class
    - Login started

--- cs alkalmazás/Projektmunka/Program.cs
+++ cs alkalmazás/Projektmunka/Program.cs
@@ -1,23 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projektmunka
 {
+    internal class User
+    {
+        public static int lastID;
+        private static List<User> users;
+        private string name;
+        private string password;
+        private int id;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value is null)
+                {
+                    throw new Exception("Username cannot be null!");
+                }
+                name = value;
+            }
+        }
+        public string PassWord
+        {
+            
+            get { return password; }
+            protected set
+            {
+                string characters = "0123456789öüóqwertzuiopőúasdfghjkléáűíyxcvbnmÖÜÓQWERTZUIOPŐÚASDFGHJKLÉÁŰÍYXCVBNM";
+                if (value is null)
+                {
+                    throw new Exception("Password cannot be null!");
+                }
+                foreach (char word in value)
+                {
+                    if (!characters.Contains(word))
+                    {
+                        throw new Exception("Password characters bust be letters of the ABC or numbers");
+                    }
+                }
+                password = value;
+            }
+        }
+
+        public int Id {
+            get { return id; }
+        }
+
+        public static List<User> Users { get { return users; } }
+
+
+        public User(string name, string pass)
+        {
+            this.Name = name;
+            this.PassWord = pass;
+            this.id = lastID++;
+        }
+        public User(string name, string pass, int id)
+        {
+            this.Name = name;
+            this.PassWord = pass;
+            this.id = id;
+        }
+        public static void AddNewUser(string uname, string upass)
+        {
+            User user = new User(uname, upass);
+            users.Add(user);
+        }
+        public static void AddExistingUser(string uname, string upass, int id)
+        {
+            User user = new User(uname, upass, id);
+            users.Add(user);
+        }
+    }
     internal class Program
     {
-        
+        void Beolvas()
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader("info.txt");
+                User.lastID = int.Parse(sr.ReadLine());
+
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+                    User.AddExistingUser(line[0], line[1], int.Parse(line[2]));
+                }
+            }
+            catch { Console.Error.WriteLine("Error during read file"); }
+            finally { if (sr!= null ) sr.Close(); }
+        }
+
+
+
         static void Main(string[] args)
         {
+            #region Fejléc
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine("\t\tKészítette: Mizere Robin Roland v0.1");
             Console.ForegroundColor = ConsoleColor.White;
+            #endregion
 
-
+            #region Menü 
             bool menu = true; // Menü aktív?
-
+            void Menu() { 
             void Reset()
             {
                 Console.ForegroundColor = ConsoleColor.White;
@@ -34,76 +129,109 @@ namespace Projektmunka
             bool regaktiv = false;
             bool settactive = false;
             bool exitactive = false;
-            
 
-            while (menu)
+
+                while (menu)
+                {
+                    switch (aktualis)
+                    {
+                        case 1:
+                            loginaktiv = true;
+                            regaktiv = false;
+                            settactive = false;
+                            exitactive = false;
+                            break;
+                        case 2:
+                            loginaktiv = false;
+                            regaktiv = true;
+                            settactive = false;
+                            exitactive = false;
+                            break;
+                        case 3:
+                            loginaktiv = false;
+                            regaktiv = false;
+                            settactive = true;
+                            exitactive = false;
+                            break;
+                        case 4:
+                            loginaktiv = false;
+                            regaktiv = false;
+                            settactive = false;
+                            exitactive = true;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (loginaktiv) { Aktiv(); }
+                    else { Reset(); }
+                    Console.WriteLine("Bejelentkezés");
+
+                    if (loginaktiv) { Aktiv(); }
+                    else { Reset(); }
+                    Console.WriteLine("Regisztráció");
+
+                    if (loginaktiv) { Aktiv(); }
+                    else { Reset(); }
+                    Console.WriteLine("Beállítások");
+
+                    if (loginaktiv) { Aktiv(); }
+                    else { Reset(); }
+                    Console.WriteLine("Bezárás");
+
+                    ConsoleKey gomb = Console.ReadKey().Key;
+
+                    if (gomb == ConsoleKey.UpArrow)
+                    {
+                        if (aktualis > 1)
+                        {
+                            aktualis--;
+                        }
+                    }
+                    else if (gomb == ConsoleKey.DownArrow)
+                    {
+                        if (aktualis < 4)
+                        {
+                            aktualis++;
+                        }
+                    }
+                    else if (gomb == ConsoleKey.Enter)
+                    {
+
+                    }
+                    
+                }
+            }
+            #endregion
+
+            Menu();
+
+            void Login()
             {
-                switch (aktualis)
+                string ReadPassword()
                 {
-                    case 1:
-                        loginaktiv = true;
-                        regaktiv = false;
-                        settactive = false;
-                        exitactive = false;
-                        break;
-                    case 2:
-                        loginaktiv = false;
-                        regaktiv   = true;
-                        settactive = false;
-                        exitactive = false;
-                        break;
-                    case 3:
-                        loginaktiv = false;
-                        regaktiv = false;
-                        settactive = true;
-                        exitactive = false;
-                        break;
-                    case 4:
-                        loginaktiv = false;
-                        regaktiv = false;
-                        settactive = false;
-                        exitactive = true;
-                        break;
-                    default:
-                        break;
-                }
+                    string rpassword = "";
+                    ConsoleKeyInfo keyInfo;
 
-                if (loginaktiv) {Aktiv();}
-                else { Reset(); }
-                Console.WriteLine("Bejelentkezés");
-
-                if (loginaktiv) { Aktiv(); }
-                else { Reset(); }
-                Console.WriteLine("Regisztráció");
-
-                if (loginaktiv) { Aktiv(); }
-                else { Reset(); }
-                Console.WriteLine("Beállítások");
-
-                if (loginaktiv) { Aktiv(); }
-                else { Reset(); }
-                Console.WriteLine("Bezárás");
-
-                ConsoleKey gomb = Console.ReadKey().Key;
-
-                if (gomb == ConsoleKey.UpArrow)
-                {
-                    if (aktualis > 1)
+                    do
                     {
-                        aktualis--;
-                    }
-                }
-                else if (gomb == ConsoleKey.DownArrow)
-                {
-                    if (aktualis<4)
-                    {
-                        aktualis++;
-                    }
-                }
-                else if (gomb == ConsoleKey.Enter)
-                {
+                        keyInfo = Console.ReadKey(true); // true: ne jelenítse meg a konzolon a beírt karaktereket
 
+                        if (keyInfo.Key != ConsoleKey.Enter)
+                        {
+                            rpassword += keyInfo.KeyChar; // Tároljuk a beírt karaktereket
+                        }
+                    } while (keyInfo.Key != ConsoleKey.Enter); // Amíg az Enter nincs lenyomva
+
+                    return rpassword;
                 }
+
+                Console.WriteLine("Kérlek add meg a Felhasználóneved!");
+                string fnev = Console.ReadLine();
+
+                Console.WriteLine("Kérlek add meg a Jelszavadat!");
+                    
+                string password = ReadPassword();
             }
 
         }
